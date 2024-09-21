df3b86d380b31b039e7afe8047205bb45de308f1
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2024-09-21 12:24:29 +0000

    adding sliders

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,11 @@ import "./Home.css";
 import myWorksData from "../../Data/MyProjects";
 import { Link } from "react-router-dom";
 import ContactUs from "../contact Us/ContactUs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 
 function Home() {
   const styles = {
@@ -18,10 +23,10 @@ function Home() {
   return (
     <>
       <div className="hero">
-        <img className="img" src={benjilo} alt="" />
+        <img className="img" src={benjilo} alt="Benjamin Dartey" />
         <h2>
-          Am Benjamin Dartey a Frontend dev based in Ghana with 2 years of
-          experience in Frontend development
+          I am Benjamin Dartey, a Frontend Developer based in Ghana with 2 years
+          of experience in Frontend development.
         </h2>
         <div className="hero-action">
           <div className="hero-connect">
@@ -33,33 +38,28 @@ function Home() {
               Connect with me
             </a>
           </div>
-          {/* <Link className="hero-resume" to="/my-resume" target="_blank">
-          My Resume
-        </Link> */}
         </div>
       </div>
       <div className="about">
         <div className="about-title">
-          <h1>About me</h1>
+          <h1>About Me</h1>
         </div>
         <div className="about-sections">
           <div className="about-left">
-            <img src={benjilo} alt="" />
+            <img src={benjilo} alt="Benjamin Dartey" />
           </div>
           <div className="about-right">
             <div className="about-para">
               <p style={{ fontSize: "18px" }}>
-                {" "}
-                A skilled and experienced Information Technology professional
-                with expertise in using HTML, CSS, Bootstrap 5, JavaScript and
-                React js to develop high-quality websites. Proficient in
-                frontend development technologies, with a proven track record of
-                delivering high-quality projects on time
+                I am a skilled and experienced Information Technology
+                professional with expertise in using HTML, CSS, Bootstrap 5,
+                JavaScript, and React.js to develop high-quality websites. I
+                have a proven track record of delivering projects on time.
               </p>
             </div>
             <div className="about-skills">
               <div className="about-skill">
-                <p>HTML&CSS</p>
+                <p>HTML & CSS</p>
                 <hr style={{ width: "90%" }} />
               </div>
               <div className="about-skill">
@@ -71,7 +71,7 @@ function Home() {
                 <hr style={{ width: "50%" }} />
               </div>
               <div className="about-skill">
-                <p>Wordpress</p>
+                <p>WordPress</p>
                 <hr style={{ width: "70%" }} />
               </div>
             </div>
@@ -95,7 +95,7 @@ function Home() {
         </div>
       </div>
 
-      {/* services */}
+      {/* Services */}
       <div className="services">
         <div className="services-title">
           <h1>My Services</h1>
@@ -107,7 +107,7 @@ function Home() {
                 <h3>{service.s_no}</h3>
                 <h2>{service.s_name}</h2>
                 <p>{service.s_desc}</p>
-                <div className="services readmore">
+                <div className="services-readmore">
                   <Link style={{ textDecoration: "none" }} to="/cvpage">
                     <p>Read More &rarr;</p>
                   </Link>
@@ -117,25 +117,51 @@ function Home() {
           })}
         </div>
       </div>
-      {/* Myprojects */}
+
+      {/* My Projects */}
       <div id="myprojects" className="mywork">
         <div className="mywork-title">
           <h1>My Projects</h1>
         </div>
-        <div className="mywork-container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
           {myWorksData.map((work, index) => (
-            <Link
-              to={`/myproject/${work.id}`}
-              key={index}
-              className="work-link" // Add custom styles to make the entire block clickable
-            >
-              <div className="work-item">
-                <img className="image" src={work.w_img} alt={work.w_name} />
-                <h1 style={{ textDecoration: "none" }}>{work.w_name}</h1>
-              </div>
-            </Link>
+            <SwiperSlide key={index}>
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/myproject/${work.id}`}
+                className="work-link"
+              >
+                <div className="work-item">
+                  <img className="image" src={work.w_img} alt={work.w_name} />
+                  <h1>{work.w_name}</h1>
+                </div>
+              </Link>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
 
       {/* Contact Us */}
